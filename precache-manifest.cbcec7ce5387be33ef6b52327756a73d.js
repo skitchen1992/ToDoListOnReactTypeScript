23db3d50117d70efe555d76be8a35bc643883359
Nik <skitchen1992@gmail.com>
2021-07-27 14:26:34 +0300

    Updates

---
+++ precache-manifest.cbcec7ce5387be33ef6b52327756a73d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a861c6abf96f2e3af697f0d4f83413",
+    "url": "/ToDoListOnReactTypeScript/index.html"
+  },
+  {
+    "revision": "3472f9a3b003fa51ac5b",
+    "url": "/ToDoListOnReactTypeScript/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "8723ccf218d52d8b8f2c",
+    "url": "/ToDoListOnReactTypeScript/static/js/2.aff1e5c9.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/ToDoListOnReactTypeScript/static/js/2.aff1e5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3472f9a3b003fa51ac5b",
+    "url": "/ToDoListOnReactTypeScript/static/js/main.bcbbe5f8.chunk.js"
+  },
+  {
+    "revision": "904f7fc67fe2936fd5fa",
+    "url": "/ToDoListOnReactTypeScript/static/js/runtime-main.0df068ff.js"
+  }
+]);
